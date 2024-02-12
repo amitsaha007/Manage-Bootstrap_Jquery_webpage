1a9078dc2b943d55856a0acc4f4de135b14529b1
Amit Saha <74258760+amitsaha007@users.noreply.github.com>
2024-02-12 14:37:08 +0600

    Bootstrap-Jquery based project

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider-cont').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: false,
+        variableWidth: true,
+        autoplay: true
+    });
+  });
+
